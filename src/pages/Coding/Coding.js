86a5b9b92461a26bcaff27252bf98271cfa87f08
Viktor <viktross736@hotmail.com>
2022-07-12 22:13:03 +0300

    add component BackToBlogButton

--- src/pages/Coding/Coding.js
+++ src/pages/Coding/Coding.js
@@ -2,13 +2,15 @@ import useGetPosts from "../../hooks/useGetPosts.js";
 import Layout from "../../components/Layout/Layout.js";
 import NewsletterContainer from "../../components/NewsletterContainer/NewsletterContainer.js";
 import PostSection from "../../components/PostSection/PostSection.js";
+import BackToBlogButton from "../../components/BackToBlogButton/BackToBlogButton.js";
 
 export default function Coding() {
   const codingPosts = useGetPosts(9)
 
   return (
     <Layout>
-      <PostSection postSectionTitle="CATEGORY" postsTopic="Coding" posts={codingPosts} onPostClick={() => {console.log('A post has been clicked!')}}/>
+      <BackToBlogButton url="/"/>
+      <PostSection postSectionTitle="CATEGORY" postsTopic="Coding" posts={codingPosts} viewAllButton={null} onPostClick={() => {console.log('A post has been clicked!')}}/>
       <NewsletterContainer />
     </Layout>
   )
